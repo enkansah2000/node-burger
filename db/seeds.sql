69b0ea3a57df81f9a382af7356f5ca175d0e7d13
Edward Nkansah <52949472+enkansah2000@users.noreply.github.com>
2020-04-28 13:59:05 -0400

    express handlebar

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('BACON DOUBLE CHEESEBURGER');
+INSERT INTO burgers (burger_name) VALUES ('HIGH PRIEST');
+INSERT INTO burgers (burger_name) VALUES ('CHEESEBURGER');
